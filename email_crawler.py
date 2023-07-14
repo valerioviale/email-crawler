3b057fad75247de41e929668498a36a6160f13d0
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-07-14 13:51:35 +0200

    Extended search keywords and waiting time
    # Find all links containing the words "contatti" or "info" or "contact" or "contacts" the href attribute
    
    Added a waiting time to let the page load

--- email_crawler.py
+++ email_crawler.py
@@ -3,6 +3,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from openpyxl import Workbook
 
@@ -21,57 +23,31 @@ for domain in domains:
         url = 'http://www.' + domain
         driver.get(url)
 
-        # Find all elements containing email addresses
-        email_elements = driver.find_elements(By.XPATH, '//a[contains(@href, "mailto:")]')
+        # Find all links containing the words "contatti" or "info" or "contact" or "contacts" the href attribute
+        contact_links = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[contains(@href, "contactos") or contains(@href, "info") or contains(@href, "contact") or contains(@href, "contacts")]'))
+        )
 
-        # Extract email addresses from elements
-        emails = [re.search(r'mailto:(.+)', element.get_attribute('href')).group(1) for element in email_elements]
+        # Iterate over the contact links and open them
+        for contact_link in contact_links:
+            contact_url = contact_link.get_attribute('href')
+            driver.get(contact_url)
 
-        # Exclude email addresses with "pec" after the "@" symbol
-        emails = [email for email in emails if not re.search(r'@pec\b', email)]
+            # Wait for the page to load and find all elements containing email addresses
+            email_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//a[contains(@href, "mailto:")]'))
+            )
 
-        # Add found email addresses to the set
-        email_set.update(emails)
+            # Extract email addresses from elements
+            for element in email_elements:
+                email = re.search(r'mailto:(.+)', element.get_attribute('href')).group(1)
+                if not re.search(r'@(?:pec|cert)\b', email, flags=re.IGNORECASE):
+                    email_set.add(email)
 
-        # Extract the second-level domain
-        domain_parts = domain.split('.')
-        if len(domain_parts) > 2:
-            second_level_domain = '.'.join(domain_parts[1:])
-            second_level_url = 'http://www.' + second_level_domain
-            driver.get(second_level_url)
-
-            # Find all elements containing email addresses in the second-level domain
-            email_elements_second_level = driver.find_elements(By.XPATH, '//a[contains(@href, "mailto:")]')
-
-            # Extract email addresses from elements in the second-level domain
-            emails_second_level = [re.search(r'mailto:(.+)', element.get_attribute('href')).group(1) for element in email_elements_second_level]
-
-            # Exclude email addresses with "pec" after the "@" symbol in the second-level domain
-            emails_second_level = [email for email in emails_second_level if not re.search(r'@pec\b', email)]
-
-            # Add found email addresses from the second-level domain to the set
-            email_set.update(emails_second_level)
-
-            # Open additional pages within the second-level domain
-            for i in range(2, 4):  # Example: open pages 2 and 3
-                additional_page_url = f'http://www.{second_level_domain}/page{i}.html'  # Modify the URL pattern as per your requirements
-                driver.get(additional_page_url)
-
-                # Find all elements containing email addresses on the additional page
-                email_elements_additional_page = driver.find_elements(By.XPATH, '//a[contains(@href, "mailto:")]')
-
-                # Extract email addresses from elements on the additional page
-                emails_additional_page = [re.search(r'mailto:(.+)', element.get_attribute('href')).group(1) for element in email_elements_additional_page]
-
-                # Exclude email addresses with "pec" after the "@" symbol on the additional page
-                emails_additional_page = [email for email in emails_additional_page if not re.search(r'@pec\b', email)]
-
-                # Add found email addresses from the additional page to the set
-                email_set.update(emails_additional_page)
     except Exception as e:
         print(f"Error processing domain '{domain}': {str(e)}")
 
-    time.sleep(6)  # Delay for 5 seconds between each domain
+    time.sleep(3)  # Delay for 3 seconds between each domain
 
 # Create an Excel workbook and sheet
 workbook = Workbook()
