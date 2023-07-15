ca0eda654be55eda6766689d66d7f993511bfac4
Valerio <viale.valerio@gmail.com>
2023-07-15 13:01:01 +0200

    Daily commit - 2023-07-15 13:01:01

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd /Users/valerio/Desktop/Courses/email-crawler
-git add test-email-crawler-30-sec
-git commit -m "Daily commit"
+
+# Log the current working directory to ensure the script runs in the correct location
+echo "Current working directory: $(pwd)"
+
+# Add all changes
+git add .
+
+# Commit changes with a timestamp
+commit_message="Daily commit - $(date +"%Y-%m-%d %H:%M:%S")"
+git commit -m "$commit_message"
+
+# Push changes
 git push
