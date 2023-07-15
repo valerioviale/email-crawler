6dd2a4cb32adfd1bb947458d84e730de78b2b021
Valerio <viale.valerio@gmail.com>
2023-07-15 15:36:12 +0200

    Daily commit - 2023-07-15 15:36:12

--- script.sh
+++ script.sh
@@ -10,10 +10,16 @@ echo "Current working directory: $(pwd)"
 # Add all changes
 git add .
 
+# Check if there are changes to commit
+if git diff --quiet --exit-code; then
+    # No changes, so stage an empty file
+    touch empty.txt
+    git add empty.txt
+fi
+
 # Commit changes with a timestamp
 commit_message="Daily commit - $(date +"%Y-%m-%d %H:%M:%S")"
 git commit -m "$commit_message"
 
 # Push changes
 git push
-
